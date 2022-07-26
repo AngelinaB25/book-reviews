38f472886f01f1b60e1839e9649446245783777b
AngelinaB25 <bestsister.a@gmail.com>
2022-07-26 16:45:55 +0300

    data base + reviews on the site

--- reviews/views.py
+++ reviews/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import redirect
 from django.shortcuts import render
 from reviews.forms import ReviewForm
+from reviews.models import Review
 def main(request):
     if request.method == 'GET':
         form = ReviewForm()
-        return render(request,'main.html',{'form':form})
+        reviews = Review.objects.all()
+        return render(request,'main.html',{'form':form, 'reviews':reviews})
     elif request.method == 'POST':
         form = ReviewForm(request.POST)
         if form.is_valid():
@@ -12,12 +14,14 @@ def main(request):
             name = data.get('name')
             email = data.get('email')
             review = data.get('review')
-            with open('data.csv','a') as file:
-                file.write(f'{name}|{email}|{review}\n')
+            rating = data.get('rating')
+            Review.objects.create( name=name,email=email,review=review,rating=rating)
+            review_1 = Review.objects.get(id = 1)
+            print(review_1.name)
             return redirect('main')
-    else:
+        reviews = Review.objects.all()
         form = ReviewForm()
-        return render(request,'main.html',{'form':form})
+        return render(request,'main.html',{'form':form, 'reviews':reviews})
 
     
 
